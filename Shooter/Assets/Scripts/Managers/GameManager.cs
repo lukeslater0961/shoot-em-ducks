cd185dc99715a010514e440e31b65b3ad3854d86
lukeslater0961 <luke.a.slater@outlook.com>
2025-01-30 00:32:42 +0100

    added a canon and target script, canon has a shooting scriupt with raycasting and target just has a takedamage function, fixed also the score issue , now it updates and the hghscore loads from playerprefs now :D

--- Shooter/Assets/Scripts/Managers/GameManager.cs
+++ Shooter/Assets/Scripts/Managers/GameManager.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] Animator settingAnimator;
-    [SerializeField] InputAction escapeAction;
-    [SerializeField] Canvas settingsCanvas;
-    [SerializeField] PlayerController playerController;
-    public bool ison = false;
+    [SerializeField] Animator           settingAnimator;
+    [SerializeField] InputAction        escapeAction;
+    [SerializeField] Canvas             settingsCanvas;
+    [SerializeField] PlayerController   playerController;
+    [SerializeField] TMP_Text           scoreText;
 
-    private int Score = 0;
+    public bool                         ison = false;
+    public int                          score = 0;
+    private int                         highScore;
 
     void Start()
     {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        //Debug.Log($"got a highscore of {highScore}");
+        scoreText.text = $"score = {score}";
         escapeAction = InputSystem.actions.FindAction("GoBack");
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -28,6 +36,7 @@ public class GameManager : MonoBehaviour
         {
             settingAnimator.SetTrigger("trigger");
             settingsCanvas.gameObject.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             ison = true;
             playerController.isInSettings = true;
@@ -36,9 +45,17 @@ public class GameManager : MonoBehaviour
         {
             settingAnimator.SetTrigger("trigger2");
             settingsCanvas.gameObject.SetActive(false);
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
             ison = false;
             playerController.isInSettings = false;
         }
+    }
+
+    public void addPoints(int type)
+    {
+        score += type * 10;
+        scoreText.text = $"score = {score}";
     }
 }
--- Shooter/Assets/Scripts/Managers/SceneLoader.cs
+++ Shooter/Assets/Scripts/Managers/SceneLoader.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
-    
+    [SerializeField] GameManager gameManager;
+
     public void LoadMainMenu(bool on)
     {
+        if (gameManager.score > PlayerPrefs.GetInt("HighScore"))
+            PlayerPrefs.SetInt("HighScore", gameManager.score);
         SceneManager.LoadSceneAsync("MainMenu");
     }
 }
